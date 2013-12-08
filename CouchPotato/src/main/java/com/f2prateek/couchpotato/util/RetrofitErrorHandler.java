a7f5bbe9f5487c7ef0ce741b7ec7dea94a8151ac
Prateek Srivastava <f2prateek@gmail.com>
2013-12-08 16:07:56 -0700

    Unused

--- CouchPotato/src/main/java/com/f2prateek/couchpotato/util/RetrofitErrorHandler.java
+++ CouchPotato/src/main/java/com/f2prateek/couchpotato/util/RetrofitErrorHandler.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2013 Prateek Srivastava (@f2prateek)
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 package com.f2prateek.couchpotato.util;
@@ -28,8 +28,6 @@ import retrofit.RetrofitError;
  * A utility class to handle and log different types of errors produced by Retrofit.
  */
 public class RetrofitErrorHandler {
-  private static final String ADDRESS_MATCHER =
-      "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
 
   /**
    * Checks the error we have, and shows a message to the user.
